8fdf243186f5dbdcb29b378e22e111af608e5952
Andre <andreluisv8@gmail.com>
2025-10-16 22:39:09 -0300

    Created class AuthController

---
+++ src/main/java/com/crud/product/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.crud.product.controller;
+
+import com.crud.product.security.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthRequest request) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
+        );
+
+        String token = jwtUtil.generateToken(request.getUsername());
+        return ResponseEntity.ok(new AuthResponse(token));
+    }
+}
