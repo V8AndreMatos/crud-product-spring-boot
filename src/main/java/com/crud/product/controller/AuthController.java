ebcccfa77a0589ac89bf0581b9dad3d8e46b2ea5
Andre <andreluisv8@gmail.com>
2025-10-16 22:42:58 -0300

    Created class AuthRequest

--- src/main/java/com/crud/product/controller/AuthController.java
+++ src/main/java/com/crud/product/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.crud.product.controller;
 
+import com.crud.product.dto.AuthRequest;
 import com.crud.product.security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
